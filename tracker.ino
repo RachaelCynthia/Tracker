656d00858a1fc8bebc514791bab1aef5d2652059
adminzolair <admin@solare.energy>
2021-07-23 18:43:17 +0100

    Updates.

--- tracker.ino
+++ tracker.ino
@@ -1,72 +1,101 @@
 #include <Adafruit_FONA.h>
 #include "Adafruit_FONA.h"
+#include <SoftwareSerial.h>
+#include <iostream>
 
 // standard pins for the shield, adjust as necessary
 #define FONA_RX 2
 #define FONA_TX 3
 #define FONA_RST 4
+
 #define mystatus "RAA status"
 #define kill "RAA kill"
 #define allow "RAA fkill"
+
 #define Emergency "08103708977"
 #define googlemap "https://maps.google.com/maps?q="
+
 uint16_t fixtime = 10000;
+
 String message="";
 String stat = "RAA:active\nLoc:";
 
 #define relay 8
 #define led 7
 
-// We default to using software serial. If you want to use hardware serial
-// (because softserial isnt supported) comment out the following three lines 
-// and uncomment the HardwareSerial line
-#include <SoftwareSerial.h>
 SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
 SoftwareSerial *fonaSerial = &fonaSS;
 
-// Hardware serial is also possible!
-//  HardwareSerial *fonaSerial = &Serial1;
-
 Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
 
-// Have a FONA 3G? use this object type instead
-//Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
-//String TriggerCase = "";
-//unsigned long intervals = 0;
 float latitude, longitude, speed_kph, heading, altitude;
 
 char fonaNotificationBuffer[64];          //for notifications from the FONA
 char smsBuffer[64];
 char* bufPtr = fonaNotificationBuffer;    //handy buffer pointer
+char callerIDbuffer[32];                  //we'll store the SMS sender number in here
 
+static const char url[] PROGMEM = "https://aepb-web-api.azurewebsites.net/api/v1/trucks/%s/locations";    // replace %s with device ID
+static const apn glo_apn[] PROGMEM = "APN";    // replace %s with device ID
+static const apn glo_password[] PROGMEM = "Flat";
+static const apn glo_username[] PROGMEM = "Flat";    
 
-boolean myLocation(){
-   boolean gps_success = false;
-   unsigned long previous = millis();
-   while(millis() - previous<fixtime){
+//char data[40] = {};
+String data = "{\"longitude\":<lat>,\"lattitude\":<lon>}";
+
+static const char ID[16] = "ABCDEFGHIKLM";
+uint16_t smslen;
+int slot;
+int charCount;
+int status_code;
+int length;
+unsigned int upload_timeout = 300;
+unsigned long last_upload_time = 0;
+
+boolean myLocation() {
+  boolean gps_success = false;
+  unsigned long previous = millis();
+  while(millis() - previous<fixtime) {
     gps_success = fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude);
     Serial.print(gps_success);
     if(gps_success) return true;
-   }
-    return false;
+  }
+  return gps_success;
 }
 
+void send_to_prunedge_server(void) {
+
+  data = data.replace("<lat>", latitude.to_string());
+  data = data.replace("<lon>", longitude.to_string());
+  char *yam = data.c_string();
+
+  if (!fona.HTTP_POST_start(url, F("application/json"), (uint8_t *)yam, strlen(yam), &statuscode, (uint16_t *)&length))
+  {
+    Serial.println("Failed to make HTTP post");
+  }
+  else {
+    Serial.println("OK");
+  }
+
+  fona.HTTP_POST_end();
+}
+
+
 void setup() {
+
   pinMode(relay, OUTPUT);
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
 
   while (! Serial);
-  //pinMode(button, INPUT);
   Serial.begin(9600);
-  //Serial.println(F("Adafruit FONA 808 & 3G GPS demo"));
-  //Serial.println(F("Initializing FONA... (May take a few seconds)"));
 
   fonaSerial->begin(9600);
   if (! fona.begin(*fonaSerial)) {
     //Serial.println(F("Couldn't find FONA"));
     while(1);
   }
+
   Serial.println(F("FONA is OK"));
   // Try to enable GPRS
   Serial.println(F("Enabling GPS..."));
@@ -74,89 +103,69 @@ void setup() {
   fonaSerial->print("AT+CNMI=2,1\r\n");
   digitalWrite(led, HIGH);
 
-  // Print SIM card IMEI number.
-  char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
-  uint8_t imeiLen = fona.getIMEI(imei);
-  if (imeiLen > 0) {
-    Serial.print("SIM card IMEI: "); Serial.println(imei);
-  }
+  fona.setGPRSNetworkSettings(glo_apn, glo_username, glo_password));
+  fona.enableGPRS(true))
+  fona.setHTTPSRedirect(true);
+
+  fona.getIMEI(ID);
 
   //fonaSerial->print("AT+CNMI=2,1\r\n");  //set up the FONA to send a +CMTI notification when an SMS is received
-
   Serial.println("FONA Ready");
+
 }
 
   
-
-
-/*bool debounce(int delayTime){
-  if (digitalRead(button)){
-    delay(delayTime);
-    if (digitalRead(button))
-      return true;
-  }
-  return false;
-}*/
-
-
-
 void loop() {
+
   char* bufPtr = fonaNotificationBuffer;    //handy buffer pointer
-  uint16_t smslen;
-  int slot;
-  int charCount;
-  char callerIDbuffer[32];  //we'll store the SMS sender number in here
-  
+
   if (fona.available())      //any data available from the FONA?
   {
     slot = 0;            //this will be the slot number of the SMS
     charCount = 0;
-    //Read the notification into fonaInBuffer
-    do  {
+  
+    do {      //Read the notification into fonaInBuffer
       *bufPtr = fona.read();
       Serial.write(*bufPtr);
       delay(1);
-    } while ((*bufPtr++ != '\n') && (fona.available()) && (++charCount < (sizeof(fonaNotificationBuffer)-1)));
-    
-    //Add a terminal NULL to the notification string
+    } while ((*bufPtr++ != '\n') && (fona.available()) && (++charCount < (sizeof(fonaNotificationBuffer) - 1)));
+
     *bufPtr = 0;
 
-    //Scan the notification string for an SMS received notification.
     //  If it's an SMS message, we'll get the slot number in 'slot'
     if (1 == sscanf(fonaNotificationBuffer, "+CMTI: " FONA_PREF_SMS_STORAGE ",%d", &slot)) {
       Serial.print("slot: "); Serial.println(slot);
 
-      // Retrieve SMS sender address/phone number.
-      if (! fona.getSMSSender(slot, callerIDbuffer, 31)) {
-        Serial.println("SMS not in slot!");
-      }
-      Serial.print(F("FROM: ")); Serial.println(callerIDbuffer);
-     // Retrieve SMS value.
-      
-      if(fona.readSMS(slot, smsBuffer, 64, &smslen)){
+      if (! fona.getSMSSender(slot, callerIDbuffer, 31)) Serial.println("SMS not in slot!");
+  
+      Serial.print(F("SMS from: ")); Serial.println(callerIDbuffer);
+  
+      if(fona.readSMS(slot, smsBuffer, 64, &smslen)) {
+        
         Serial.println(smsBuffer);
-            
-        if( strstr(smsBuffer, mystatus) == 0 ) {
-          if(myLocation()){
+        
+        /* Remote commands are executed here
+         * See definitions above.
+        */
+        if( strstr(smsBuffer, F(mystatus)) == 0 ) {
+          if(myLocation()) {
             message = googlemap + String(latitude,6)+","+String(longitude,6);
             message = stat + message +"\nSpeed:"+ (String)speed_kph + "KPH";
-            if(!fona.sendSMS(callerIDbuffer, message.c_str()))
-             Serial.println(F("Failed to send respons"));
+            if(!fona.sendSMS(callerIDbuffer, message.c_str())) Serial.println(F("Failed to send respons"));
           }
-          else{
+          else {
             Serial.println(F("Response success"));
           }
-        }
-          
-          else if (strstr(smsBuffer, kill) == 0) {
-            stat = "RAA: killed\nLoc:";
-            digitalWrite(relay, HIGH);
-            Serial.println("vTrack stoped!");
-            if (!fona.sendSMS(callerIDbuffer, "RAA:killed")) {
-              Serial.println(F("Failed"));
+        } 
+        else if (strstr(smsBuffer, F(kill)) == 0) {   // relay handler
+          stat = "RAA: killed\nLoc:";
+          digitalWrite(relay, HIGH);
+          Serial.println("vTrack stoped!");
+          if (!fona.sendSMS(callerIDbuffer, F("RAA:killed"))) {
+            Serial.println(F("Realay resp: Failed"));
           }
         }
-        else if (strstr(smsBuffer, allow) == 0) {
+        else if (strstr(smsBuffer, F(allow)) == 0) {
           digitalWrite(relay, LOW);
           //control = false;
           stat = "RAA: active\nLoc:";
@@ -166,5 +175,10 @@ void loop() {
         }
       }
     }
+  }
+
+  if (millis() - last_upload_time > upload_timeout) {
+    last_upload_time = millis();
+    send_to_prunedge_server();
   }
 }
